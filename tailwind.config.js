d0b70c22f87f52ed319d4d6f8abebddae5d82974
Utkarshsaxena7 <68659707+Utkarshsaxena7@users.noreply.github.com>
2023-07-04 22:57:54 +0530

    Project
    cd

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {screens:{
+      xsm: "320px",
+    },
+    colors: {
+      primary1 :'#0067b8',
+      primary2 :'#035697',
+    },
+  },
+  },
+  plugins: [],
+}
+
